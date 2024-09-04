79a5ca21f893e088162051317f91cd575d58a6aa
gxddem <gxddem@gmail.com>
2024-09-05 02:14:01 +0300

    1

--- main.go
+++ main.go
@@ -1,21 +1,26 @@
 package main
 
 import (
-    "log"
+	"log"
 
-    "github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3"
 )
 
 func main() {
-    // Initialize a new Fiber app
-    app := fiber.New()
+	// Initialize a new Fiber app
+	app := fiber.New()
 
-    // Define a route for the GET method on the root path '/'
-    app.Get("/", func(c fiber.Ctx) error {
-        // Send a string response to the client
-        return c.SendString("Hello, World 👋!")
-    })
+	// Define a route for the GET method on the root path '/'
+	app.Get("/", func(d fiber.Ctx) error {
+		// Send a string response to the client
+		return d.SendString("Hello, World 👋!")
+	})
 
-    // Start the server on port 3000
-    log.Fatal(app.Listen(":3000"))
+	app.Post("/", func(d fiber.Ctx) error {
+
+		return d.SendString(" Hello, POST👋!")
+	})
+
+	// Start the server on port 3000
+	log.Fatal(app.Listen(":3000"))
 }
