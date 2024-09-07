495ff4c4abab2e8d89d46752f01902fbda287d54
va1ker <dead1nside.inside@yandex.ru>
2024-09-07 03:07:32 +0300

    fix PUT and GET methods

--- main.go
+++ main.go
@@ -19,11 +19,9 @@ func main() {
 	app := fiber.New()
 
 	app.Get("/user", func(c fiber.Ctx) error {
-		userid := c.Query("userid")
-		return c.SendString("Hello," + userid)
-	})
-
-	app.Get("/user", func(c fiber.Ctx) error {
+		if userid := c.Query("userid"); userid != "" {
+			return c.SendString("Hello " + userid)
+		}
 		return c.SendString("Hello, World 👋!")
 	})
 
@@ -40,15 +38,24 @@ func main() {
 	})
 
 	app.Put("/user", func(c fiber.Ctx) error {
-		//var userData User
-		//response := c.Queries()
-
+		var request Request
 		userid := c.Query("userid")
 		if userid == "" {
 			return c.Status(fiber.StatusBadRequest).SendString(fiber.ErrBadRequest.Error())
 		}
+		
+		if err := c.Bind().Body(&request); err != nil {
+			return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+		}
+		
 
-		return c.SendString(userid)
+		return c.JSON(
+			fiber.Map{
+				"userid":userid,
+				"username":request.Username,
+				"email":request.Mail,
+
+			})
 	})
 
 	app.Delete("/user", func(c fiber.Ctx) error {
@@ -56,7 +63,7 @@ func main() {
 		if userid == "" {
 			return c.Status(fiber.StatusBadRequest).SendString(fiber.ErrBadRequest.Error())
 		}
-		return c.SendString("1123")
+		return c.SendString("Delete " + userid)
 	})
 
 	log.Fatal(app.Listen(":3000"))
