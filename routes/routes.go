093d5076d5fba85070a845ceaed7ba00e7a74dfe
va1ker <dead1nside.inside@yandex.ru>
2024-09-08 16:08:50 +0300

    add structure

---
+++ routes/routes.go
@@ -0,0 +1,17 @@
+package routes
+
+import (
+	"github.com/gofiber/fiber/v3"
+
+    "audio-stream-golang/handlers"
+)
+
+func SetupRoutes(app *fiber.App) {
+    api := app.Group("/api")
+
+    // Пример маршрута для пользователей
+    api.Get("/users", handlers.GetUser)
+    api.Post("/users", handlers.CreateUser)
+	api.Post("/users", handlers.UpdateUser)
+	api.Delete("/users", handlers.DeleteUser)
+}
