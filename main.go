645fbb7b8484e0ff4e1de619cab53fa81a40e085
gxddem <gxddem@gmail.com>
2024-09-09 23:01:33 +0300

    123

--- main.go
+++ main.go
@@ -4,24 +4,29 @@ import (
 	"audio-stream-golang/config"
 	_ "audio-stream-golang/docs"
 	"audio-stream-golang/routes"
+	"fmt"
 	"log"
+
 	"github.com/gofiber/fiber/v2"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func main() {
-	app := fiber.New()
 	EnvConfig := config.GetConfig()
-	routes.SetupUserRoutes(app)
-	routes.SetupSwagger(app)
-	log.Fatal(app.Listen(EnvConfig.App_ip + ":" + EnvConfig.App_port))
-
 	dsn := "host=" + EnvConfig.Postgres_host +
 		" user=" + EnvConfig.Postgres_user +
 		" password=" + EnvConfig.Postgres_password +
 		" dbname=" + EnvConfig.Postgres_db +
 		" dbPort=" + EnvConfig.Postgres_port
+	Database, err := gorm.Open(postgres.Open(dsn))
+	if err != nil {
+		fmt.Println("couldn't connect to database", err)
+		return
+	}
+	app := fiber.New()
+	routes.SetupUserRoutes(app)
+	routes.SetupSwagger(app)
+	log.Fatal(app.Listen(EnvConfig.App_ip + ":" + EnvConfig.App_port))
 
-	gorm.Open(postgres.Open(dsn))
 }
