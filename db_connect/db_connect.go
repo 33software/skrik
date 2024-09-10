e8c0efa3289d871ba1b0d9143baee14313234c97
gxddem <gxddem@gmail.com>
2024-09-10 18:06:41 +0300

    moved db setup part into the new package (it didn't help)

---
+++ db_connect/db_connect.go
@@ -0,0 +1,26 @@
+package db_connect
+
+import (
+	"audio-stream-golang/config"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func SetupDb() {
+	EnvConfig := config.GetConfig()
+	dsn := "host=" + EnvConfig.Postgres_host +
+		" user=" + EnvConfig.Postgres_user +
+		" password=" + EnvConfig.Postgres_password +
+		" dbname=" + EnvConfig.Postgres_db +
+		" port=" + EnvConfig.Postgres_port
+	Database, err := gorm.Open(postgres.Open(dsn))
+	if err != nil {
+		log.Println("couldn't connect to database", err)
+		return
+	}
+	var result int64
+	Database.Raw("SELECT 1123123123123123").Scan(&result)
+	log.Println(result)
+}
--- main.go
+++ main.go
@@ -2,33 +2,19 @@ package main
 
 import (
 	"audio-stream-golang/config"
+	"audio-stream-golang/db_connect"
 	_ "audio-stream-golang/docs"
 	"audio-stream-golang/routes"
 	"log"
 
 	"github.com/gofiber/fiber/v2"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
 )
 
 func main() {
 	EnvConfig := config.GetConfig()
-	dsn := "host=" + EnvConfig.Postgres_host +
-		" user=" + EnvConfig.Postgres_user +
-		" password=" + EnvConfig.Postgres_password +
-		" dbname=" + EnvConfig.Postgres_db +
-		" port=" + EnvConfig.Postgres_port
-	Database, err := gorm.Open(postgres.Open(dsn))
-	if err != nil {
-		log.Println("couldn't connect to database", err)
-		return
-	}
 	app := fiber.New()
 	routes.SetupUserRoutes(app)
 	routes.SetupSwagger(app)
 	log.Fatal(app.Listen(EnvConfig.App_ip + ":" + EnvConfig.App_port))
-
-	var result int64
-	Database.Raw("SELECT 1123123123123123").Scan(&result)
-	log.Println(result)
+	db_connect.SetupDb()
 }
