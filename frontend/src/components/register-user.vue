7bff30222720791278a6d58142595b9bfb35784b
va1ker <dead1nside.inside@yandex.ru>
2024-10-08 23:49:13 +0300

    init front

---
+++ frontend/src/components/register-user.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h2>Register</h2>
+    <form @submit.prevent="registerUser">
+      <input v-model="email" type="email" placeholder="Email" required />
+      <input v-model="password" type="password" placeholder="Password" required />
+      <button type="submit">Register</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+    };
+  },
+  methods: {
+    async registerUser() {
+      const response = await fetch('http://localhost:8080/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: this.email,
+          password: this.password,
+        }),
+      });
+      const data = await response.json();
+      if (data.success) {
+        this.$router.push('/login');
+      } else {
+        alert('Registration failed');
+      }
+    },
+  },
+};
+</script>
---
+++ frontend/src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HelloWorld from '../components/hello-world.vue'
+import Login from '../components/login-user.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: HelloWorld,
+  },
+  {
+    path: 'login/',
+    name: 'Login',
+    component: Login,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
